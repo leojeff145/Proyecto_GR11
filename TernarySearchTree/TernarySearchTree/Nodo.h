3138f27a3fe7969edd1c2a00b97e15b2dd1cd7a7
Leonardo <jlobando2@espe.edu.ec>
2023-01-13 23:05:34 -0500

    arreglo 1.0

---
+++ TernarySearchTree/TernarySearchTree/Nodo.h
@@ -0,0 +1,90 @@
+#pragma once
+
+template<typename T>
+class Nodo
+{
+public:
+    Nodo(T dato);
+<<<<<<< HEAD
+    Nodo(T dato, Nodo<T> *siguiente);
+    Nodo(T dato, Nodo<T> *siguiente, Nodo<T> *anterior);
+=======
+    Nodo(T dato, Nodo<T>* siguiente);
+    Nodo(T dato, Nodo<T>* siguiente, Nodo<T>* anterior);
+
+    void set_siguiente(Nodo<T>* siguiente);
+    Nodo<T>* get_siguiente();
+>>>>>>> 33ff88e141b50845d830051e53773676c577ebdc
+
+    void set_siguiente(Nodo<T> *siguiente);
+    Nodo<T> *get_siguiente();
+
+    void set_anterior(Nodo<T> *siguiente);
+    Nodo<T> *get_anterior();
+
+    void set_dato(T dato);
+    T get_dato();
+
+private:
+    T dato;
+<<<<<<< HEAD
+    Nodo<T> *siguiente = nullptr;
+    Nodo<T> *anterior = nullptr;
+};
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+template<typename T>
+Nodo<T>::Nodo(T dato) {
+    this->dato = dato;
+    this->siguiente = nullptr;
+    this->anterior = nullptr;
+}
+
+template<typename T>
+Nodo<T>::Nodo(T dato, Nodo<T> *siguiente) {
+    this->dato = dato;
+    this->siguiente = siguiente;
+    this->anterior = nullptr;
+}
+
+template<typename T>
+Nodo<T>::Nodo(T dato, Nodo<T> *siguiente, Nodo<T> *anterior) {
+    this->dato = dato;
+    this->siguiente = siguiente;
+    this->anterior = anterior;
+}
+
+template<typename T>
+void Nodo<T>::set_dato(T dato) {
+    this->dato = dato;
+}
+
+template<typename T>
+T Nodo<T>::get_dato() {
+    return dato;
+}
+
+template<typename T>
+void Nodo<T>::set_siguiente(Nodo<T> *siguiente) {
+    this->siguiente = siguiente;
+}
+
+template<typename T>
+Nodo<T>* Nodo<T>::get_siguiente() {
+    return siguiente;
+}
+
+template<typename T>
+void Nodo<T>::set_anterior(Nodo<T> *anterior) {
+    this->anterior = anterior;
+}
+
+template<typename T>
+Nodo<T>* Nodo<T>::get_anterior() {
+    return anterior;
+}
+=======
+    Nodo<T>* siguiente = nullptr;
+    Nodo<T>* anterior = nullptr;
+};
+>>>>>>> 33ff88e141b50845d830051e53773676c577ebdc
