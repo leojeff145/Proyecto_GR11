1e6bdff5ded8c690881cf66380d2b205734ee30c
wendy <waquintana@espe.edu.ec>
2023-01-13 21:57:23 -0500

    Cree Nodo

--- TernarySearchTree/TernarySearchTree/Nodo.h
+++ TernarySearchTree/TernarySearchTree/Nodo.h
@@ -1,1 +1,24 @@
 #pragma once
+
+template<typename T>
+class Nodo
+{
+public:
+	Nodo(T dato);
+	Nodo(T dato, Nodo<T> *siguiente);
+	Nodo(T dato, Nodo<T> *siguiente, Nodo<T> *anterior);
+	
+    void set_siguiente(Nodo<T>* siguiente);
+    Nodo<T>* get_siguiente();
+
+    void set_anterior(Nodo<T>* siguiente);
+    Nodo<T>* get_anterior();
+
+    void set_dato(T dato);
+    T get_dato();
+
+private:
+    T dato;
+    Nodo<T>* siguiente = nullptr;
+    Nodo<T>* anterior = nullptr;
+}
