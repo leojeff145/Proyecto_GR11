384e07a3b209ab83e546b82c89fa388468fcd3a5
WilliamEspe <waleon@espe.edu.ec>
2023-01-13 23:22:05 -0500

    nodosi

--- TernarySearchTree/TernarySearchTree/NodoBasico.h
+++ TernarySearchTree/TernarySearchTree/NodoBasico.h
@@ -1,5 +1,25 @@
 #pragma once
-class NodoBasico
-{
-};
+#include <string>
+#include <list>
 
+class NodoBasico {
+public:
+	NodoBasico();
+	NodoBasico(std::string dato);
+
+	virtual ~NodoBasico();
+
+	void nodosHijos(NodoBasico* nd);
+	std::list<NodoBasico*> getHijos();
+
+	std::string getDato();
+	void setDato(std::string dato);
+
+protected:
+	void setPadre(NodoBasico* nd);
+
+	std::string _dato;
+
+	std::list<NodoBasico*> _hijos;
+	NodoBasico* _padre;
+};
