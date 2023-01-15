de7473c7a90f0cdb9c1231ae9eb07b7f203cfab4
wendy <waquintana@espe.edu.ec>
2023-01-14 23:59:23 -0500

    Cree MenuOpciones

--- TernarySearchTree/TernarySearchTree/Menu_Opciones.cpp
+++ TernarySearchTree/TernarySearchTree/Menu_Opciones.cpp
@@ -27,30 +27,3 @@ MenuOpciones::MenuOpciones(std::string label, MenuOpcionesCallback callback, Men
 
 
 
-fecha
-#pragma once
-#include <string>
-
-class Fecha
-{
-public:
-    Fecha();
-    Fecha(int dia, int mes, int anio);
-
-    int getDia() const;
-    void setDia(int dia);
-    int getMes() const;
-    void setMes(int mes);
-    int getAnio() const;
-    void setAnio(int anio);
-
-    static Fecha actual();
-    static Fecha leer(std::string mensaje);
-    static bool valida(int dia, int mes, int anio);
-    static bool valida(Fecha fecha);
-
-private:
-    int dia = 0;
-    int mes = 0;
-    int anio = 0;
-};
