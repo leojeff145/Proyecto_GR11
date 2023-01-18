537c03ed3a99294bfcf15c5abe0314ce5f926bf2
WilliamEspe <waleon@espe.edu.ec>
2023-01-17 23:29:37 -0500

    gestorappca

--- TernarySearchTree/TernarySearchTree/Gestor_app.cpp
+++ TernarySearchTree/TernarySearchTree/Gestor_app.cpp
@@ -8,5 +8,53 @@
 #pragma execution_character_set( "utf-8" )
 
 int Gestor_app::iniciar() {
+    SetConsoleOutputCP(65001);
+
+}
+
+void Gestor_app::correr() {
+	Menu menu("Menu principal");
+	menu.add_opciones(MenuOpciones("Arbol Ternario", [&]() {
+		menu_opArbol();
+		}, false));
+	menu.add_opciones(MenuOpciones("Generar PDF", [&]() {
+		generarPDF();
+		}));
+	menu.add_opciones(MenuOpciones("Ayuda", [&]() {
+		ShellExecuteA(NULL, "open", "manual.chm", NULL, NULL, SW_SHOW);
+		}));
+	menu.add_opciones(menu.opciones_salir("Salir"));
+	menu.mostrar();
+}
+
+void Gestor_app::generarPDF() {
+	time_t tiempo = time(0);
+	struct tm* nuevo = localtime(&tiempo);
+	std::string tiempot = std::to_string(nuevo->tm_year + 1900) + std::to_string(nuevo->tm_mon + 1) + std::to_string(nuevo->tm_mday) + std::to_string(nuevo->tm_hour) + std::to_string(nuevo->tm_min) + std::to_string(nuevo->tm_sec);
+	std::stringstream ss;
 	
+	ss  << std::setfill('0') << std::setw(2) << nuevo->tm_mday << "-"
+		<< std::setfill('0') << std::setw(2) << nuevo->tm_mon + 1 << "-"
+		<< std::setfill('0') << std::setw(4) << nuevo->tm_year + 1900 << "-"
+		<< std::setfill('0') << std::setw(2) << nuevo->tm_hour + 1 << ":" << std::setw(2) << nuevo->tm_min;
+	
+	std::string subfijo = tiempot;
+	std::string insertar = "doc_" + subfijo + ".html";
+	std::string extraer = "reporte_" + subfijo + ".pdf";
+	std::ofstream enArchivo(insertar);
+	std::stringstream enBusca;
+
+	enBusca << "<html><body><h1 style=\"text-align: center\">Universidad de las fuerzas armadas ESPE</h1><br><h2>Arbol ternario - Reporte</h2><br>";
+	enBusca << "<strong>Fecha:  </strong>" << ss.str() << "<br><br><br>";
+	enBusca	<< "<strong>Ternary search tree inorder: </strong> ";
+
+	arbol->atravesar([&enBusca](char c) {
+		enBusca << "<strong style=\"background:#ddd; padding: 4px; margin-right: 5px\">" << c << "</strong>";
+		});
+	
+	enBusca << "</body><html>";
+	enArchivo << enBusca.str();
+	enArchivo.close();
+
+	system((std::string("pdf ./") + insertar + "./" +extraer).c_str());
 }
