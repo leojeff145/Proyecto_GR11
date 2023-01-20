b181c3eb6e5e39b6362e4e7b5e22690a2e98d23a
Leonardo <jlobando2@espe.edu.ec>
2023-01-20 06:44:12 -0500

    unit test

---
+++ TernarySearchTree/PruebasUnitarias/PruebasUnitarias.cpp
@@ -0,0 +1,21 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "../utils.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace PruebasUnitarias
+{
+	TEST_CLASS(PruebasUnitarias)
+	{
+	public:
+		
+		TEST_METHOD(TestUtils)
+		{
+			Assert::IsFalse(Validaciones::cedula_valida("1726744295"));
+			Assert::IsTrue(Validaciones::cedula_valida("1726744293"));
+            
+			Assert::AreEqual(Validaciones::trim("  abc  ").c_str(), "abc");
+		}
+	};
+}
